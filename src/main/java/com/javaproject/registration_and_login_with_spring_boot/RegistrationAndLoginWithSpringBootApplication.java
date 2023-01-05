19a643d5a85211c7b471cb5deed96728ff50fa89
xinyao qian <zceexqi@ucl.ac.uk>
2023-01-05 23:30:57 +0000

    Initial commit

---
+++ src/main/java/com/javaproject/registration_and_login_with_spring_boot/RegistrationAndLoginWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.registration_and_login_with_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationAndLoginWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationAndLoginWithSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaproject/registration_and_login_with_spring_boot/RegistrationAndLoginWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.registration_and_login_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAndLoginWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
