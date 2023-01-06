ffacfd016ec6ce0cdc857b6f8eebceaec097fbce
xinyao qian <zceexqi@ucl.ac.uk>
2023-01-06 00:02:39 +0000

    Created Role Class; Set up many to many relationship mbetween these two.

--- src/main/java/com/javaproject/registration_and_login_with_spring_boot/model/User.java
+++ src/main/java/com/javaproject/registration_and_login_with_spring_boot/model/User.java
@@ -20,6 +20,14 @@ public class User {
     private String email;
     private String password;
     // one user has multiple roles
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"))
+
     private Collection<Role> roles;
 
     public User(String firstName, String lastName, String email, String password, Collection<Role> roles) {
