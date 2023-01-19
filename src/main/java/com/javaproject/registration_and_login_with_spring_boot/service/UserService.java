c8ff795d32f3b6aad7ee9633a4245b5e556a5ece
xinyao qian <zceexqi@ucl.ac.uk>
2023-01-19 22:25:10 +0000

    debug

--- src/main/java/com/javaproject/registration_and_login_with_spring_boot/service/UserService.java
+++ src/main/java/com/javaproject/registration_and_login_with_spring_boot/service/UserService.java
@@ -1,5 +1,5 @@
 package com.javaproject.registration_and_login_with_spring_boot.service;
-
+import org.springframework.security.core.userdetails.UserDetailsService;
 import com.javaproject.registration_and_login_with_spring_boot.model.User;
 import com.javaproject.registration_and_login_with_spring_boot.web.dto.UserRegistrationDto;
 
--- src/main/java/com/javaproject/registration_and_login_with_spring_boot/web/UserRegistrationController.java
+++ src/main/java/com/javaproject/registration_and_login_with_spring_boot/web/UserRegistrationController.java
@@ -4,7 +4,9 @@ package com.javaproject.registration_and_login_with_spring_boot.web;
 import com.javaproject.registration_and_login_with_spring_boot.service.UserService;
 import com.javaproject.registration_and_login_with_spring_boot.web.dto.UserRegistrationDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,10 +23,18 @@ public class UserRegistrationController {
 
 
     // handler ()里面没看懂
+    @PostMapping
     public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto){
         userService.save(registrationDto);
         return "redirect:/registration?success";
 
+    }
+
+    // shortcut for @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    public String showRegistrationForm() {
+
+        return "registration";
     }
 
 
